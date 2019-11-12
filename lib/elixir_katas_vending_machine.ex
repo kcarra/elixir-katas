6a11655edd213664da5adbba7abe8d0525c89e9b
Kevin Carrabine <CarrabineConsulting@outlook.com>
2019-11-12 16:52:20 -0500

    Ran formatting on vending machine to make it more readable

--- lib/elixir_katas_vending_machine.ex
+++ lib/elixir_katas_vending_machine.ex
@@ -1,5 +1,8 @@
 defmodule ElixirKatas.VendingMachine.State do
-  defstruct current_amount: 0.00, coin_return: 0.00, msg: "", stock: %{"COLA" => 10, "CHIPS" => 10, "CANDY" => 10}
+  defstruct current_amount: 0.00,
+            coin_return: 0.00,
+            msg: "",
+            stock: %{"COLA" => 10, "CHIPS" => 10, "CANDY" => 10}
 end
 
 defmodule ElixirKatas.VendingMachine do
@@ -26,28 +29,53 @@ defmodule ElixirKatas.VendingMachine do
   end
 
   def insert_coin(state), do: %{state | :msg => @insert_coin_msg}
-  def insert_coin(state, coin) when coin != "PENNY", do: update_state(state, coin, :current_amount)
+
+  def insert_coin(state, coin) when coin != "PENNY",
+    do: update_state(state, coin, :current_amount)
+
   def insert_coin(state, coin), do: update_state(state, coin, :coin_return)
 
   def select_product(state, product) do
     cond do
       state[:stock][product] == 0 ->
         %{state | :msg => @sold_out}
+
       state[:current_amount] == @vending_machine_products[product] ->
-        %{state | :msg => @thank_you, :current_amount => 0.00, :stock => %{state[:stock] | product => state[:stock][product] - 1}}
+        %{
+          state
+          | :msg => @thank_you,
+            :current_amount => 0.00,
+            :stock => %{state[:stock] | product => state[:stock][product] - 1}
+        }
+
       state[:current_amount] == 0 ->
         %{state | :msg => @insert_coin_msg, :current_amount => 0.00}
+
       state[:current_amount] < @vending_machine_products[product] ->
         %{state | :msg => "PRICE #{@vending_machine_products[product]}"}
+
       state[:current_amount] > @vending_machine_products[product] ->
-        %{state | :msg => "", :current_amount => 0.00, :coin_return => Float.round(state[:current_amount] - @vending_machine_products[product], 2), :stock => %{state[:stock] | product => state[:stock][product] - 1}}
-      true -> 
+        %{
+          state
+          | :msg => "",
+            :current_amount => 0.00,
+            :coin_return =>
+              Float.round(state[:current_amount] - @vending_machine_products[product], 2),
+            :stock => %{state[:stock] | product => state[:stock][product] - 1}
+        }
+
+      true ->
         state
     end
   end
 
   def return_coins(state) do
-      %{state | :coin_return => state[:current_amount], :current_amount => 0.00, :msg => @insert_coin_msg}
+    %{
+      state
+      | :coin_return => state[:current_amount],
+        :current_amount => 0.00,
+        :msg => @insert_coin_msg
+    }
   end
 
   def update_state(state, coin, key_to_update) do
